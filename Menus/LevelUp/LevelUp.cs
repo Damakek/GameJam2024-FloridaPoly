497b44fe98f0b0eff2f6d997c104e46103501e17
Damakek <55289884+Damakek@users.noreply.github.com>
2024-02-25 13:49:22 -0500

    font for player
    - font for player stats applied
    - 2 new drugs added
    - level up drugs randomized

--- Menus/LevelUp/LevelUp.cs
+++ Menus/LevelUp/LevelUp.cs
@@ -8,9 +8,15 @@ using UnityEngine.UI;
 public class LevelUp : MonoBehaviour
 {
 
+    public List<GameObject> drugs = new List<GameObject>();
+
     public GameObject drugA;
     public GameObject drugB;
     public GameObject drugC;
+
+    public Text drugA_description;
+    public Text drugB_description;
+    public Text drugC_description;
 
     public GameObject levelUpScreen;
     public GameObject player;
@@ -18,6 +24,42 @@ public class LevelUp : MonoBehaviour
     private void Awake()
     {
         levelUpScreen.SetActive(false);
+    }
+
+    public void Start()
+    {
+        int rand1 = Random.Range(1, drugs.Count);
+        int rand2 = rand1;
+        int rand3 = rand1;
+
+        while(rand2 == rand1)
+        {
+            rand2 = Random.Range(1, drugs.Count);
+        }
+
+        while(rand3 == rand1 || rand3 == rand2)
+        {
+            rand3 = Random.Range(1, drugs.Count);
+        }
+
+       if(drugA == null)
+       {
+            drugA = drugs[rand1];
+
+            drugA_description.text = drugA.gameObject.GetComponent<drug>().plus + "+\n" + drugA.gameObject.GetComponent<drug>().minus + "-";
+       }
+       if(drugB == null)
+       {
+            drugB = drugs[rand2];
+
+            drugB_description.text = drugB.gameObject.GetComponent<drug>().plus + "+\n" + drugB.gameObject.GetComponent<drug>().minus + "-";
+        }
+       if(drugC == null)
+       {
+            drugC = drugs[rand3];
+
+            drugC_description.text = drugC.gameObject.GetComponent<drug>().plus + "+\n" + drugC.gameObject.GetComponent<drug>().minus + "-";
+        }
     }
 
     public void drug_1()
